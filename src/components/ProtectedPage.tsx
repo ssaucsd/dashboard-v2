2de999a86dcefe42a36661d3998cf6eb43fdcdc7
Daniel Xu <hi@danielx.me>
2025-12-22 23:22:36 -0800

    readd server.ts

--- src/components/ProtectedPage.tsx
+++ src/components/ProtectedPage.tsx
@@ -2,9 +2,8 @@ import { createClient } from '@/lib/supabase/server';
 import { redirect } from 'next/navigation';
 
 export default async function ProtectedPage({ children }: { children: React.ReactNode }) {
-    const supabase = createClient();
-    const auth = supabase.auth;
-    const { data: { session } } = await auth.getSession();
+    const supabase = await createClient();
+    const { data: { session } } = await supabase.auth.getSession();
 
     if (!session) {
         redirect('/auth');
